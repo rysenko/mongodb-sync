0376584699758bd48052b8c758ab6949df0a1c0f
Thomas de Barochez <thomas.barochez+github@gmail.com>
2016-03-01 14:31:14 +0100

    restore, fix usage of MONGODB_RESTORE_DB

--- restore.sh
+++ restore.sh
@@ -11,14 +11,14 @@ MONGODB_PASS=${MONGODB_RESTORE_PASS:-${MONGODB_RESTORE_ENV_MONGODB_PASS}}
 
 [[ ( -n "${MONGODB_RESTORE_USER}" ) ]] && USER_RESTORE_STR=" --username ${MONGODB_RESTORE_USER}"
 [[ ( -n "${MONGODB_RESTORE_PASS}" ) ]] && PASS_RESTORE_STR=" --password ${MONGODB_RESTORE_PASS}"
-[[ ( -n "${MONGODB_RESTORE_DB}" ) ]] && USER_RESTORE_STR=" --db ${MONGODB_RESTORE_DB}"
+[[ ( -n "${MONGODB_RESTORE_DB}" ) ]] && DB_RESTORE_STR=" --db ${MONGODB_RESTORE_DB}"
 
 FILE_TO_RESTORE=${1}
 
 [[ -z "${1}" ]] && FILE_TO_RESTORE=$(ls /backup -N1 | grep -iv ".tgz" | sort -r | head -n 1)
 
 echo "=> Restore database from ${FILE_TO_RESTORE}"
-if mongorestore --host ${MONGODB_RESTORE_HOST} --port ${MONGODB_RESTORE_PORT} ${USER_RESTORE_STR}${PASS_RESTORE_STR} /backup/$FILE_TO_RESTORE; then
+if mongorestore --drop --host ${MONGODB_RESTORE_HOST} --port ${MONGODB_RESTORE_PORT} ${USER_RESTORE_STR}${PASS_RESTORE_STR}${DB_RESTORE_STR} /backup/$FILE_TO_RESTORE; then
     echo "   Restore succeeded"
 else
     echo "   Restore failed"
